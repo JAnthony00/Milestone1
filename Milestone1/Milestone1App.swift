331b623e06d6156b19849be1ed0cdf71888c2fb7
Jack Brighton <jack.brighton@griffithuni.edu.au>
2022-03-30 15:34:16 +1000

    Initial Commit

---
+++ Milestone1/Milestone1App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone1App.swift
+//  Milestone1
+//
+//  Created by Jack Brighton on 30/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Milestone1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
